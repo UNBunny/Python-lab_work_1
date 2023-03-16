de269b5183a991f657d03ecd9815dddd3e2a010d
UNBunny <112119548+UNBunny@users.noreply.github.com>
2023-03-17 01:53:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-from collections import defaultdict
 
-# Отправляем GET-запрос на страницу с лучшими фильмами imdb.com
-k = 1
-d = dict()
-while k <= 5:
-    if k == 1:
-        url = "https://www.chitai-gorod.ru/search?phrase=python"
-    else:
-        url = 'https://www.chitai-gorod.ru/search?phrase=python&page=' + str(k)
+url = 'https://www.omgtu.ru/general_information/the-structure/the-department-of-university.php'
+page = requests.get(url) # Выполняет GET-запрос к веб-странице и возвращает ответ сервера, который сохраняется в переменной
+print(page.status_code) # Выводит HTTP-статус код ответа сервера на консоль.
 
-    page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser') # Создает объект класса BeautifulSoup,# который используется для парсинга содержимого страницы
+departments = soup.find('div', class_='main__content').findAll('a') # Находит все ссылки на странице
 
-    # Создаем объект BeautifulSoup для парсинга HTML-страницы
-    soup = BeautifulSoup(page.text, 'html.parser')
-    book = soup.findAll(class_='product-title__head')
-    author = soup.findAll(class_='product-title__author')
-    price = soup.findAll(class_='product-price__value')
-    price_temp = soup.findAll(class_='product-card__price product-card__row')
-    for el in range(len(book)):
-        temp = price_temp[el].find(class_='product-price__value').text if price_temp[el].find(class_='product-price__value') != None else " "
-        d['Name'] = d.get('Name', []) + [book[el].text]
-        d['Author'] = d.get('Author', []) + [author[el].text]
-        d['Price'] = d.get('Price', []) + [temp.replace('\xa0', ' ')]
-    k += 1
-df = pd.DataFrame(d)
-df.to_excel('./teams.xlsx')
+file = open('departments.txt', 'w')
+i = 1
+for item in departments: #  Начинает цикл, который перебирает все ссылки, сохраненные в переменной departments.
+    file.write(str(i) + '. ' + item.text + '\n')
+    i += 1
